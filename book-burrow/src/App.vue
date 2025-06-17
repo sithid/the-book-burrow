7891e634f9795bd0a53bf5b13199c6bca5ae194b
James Glosser <demonicurges05@gmail.com>
2025-06-17 04:29:01 -0400

    vue 3 project setup

---
+++ book-burrow/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <h1>Welcome</h1>
+    <nav class="nav-menu">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+  .header {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    margin: 0 10px;
+    width: 100%;
+  }
+
+  .nav-menu {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    justify-content: center;
+  }
+</style>
---
+++ book-burrow/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="counter-container">
+    <h1>Count: {{ counterStore.count }}</h1>
+    <button @click="counterStore.increment()">Increment</button>
+  </div>
+</template>
+
+<script setup>
+  import { useCounterStore } from '../stores/counter.js';
+  const counterStore = useCounterStore();
+</script>
+
+<style scoped>
+  .counter-container {
+    justify-content: center;
+    text-align: center;
+  }
+
+  .counter-container button {
+    max-width: 100px;
+    max-height: 50px;
+  }
+</style>
